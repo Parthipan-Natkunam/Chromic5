7843a608ff287b0430a5d19b62d285be7a50bea2
Parthipan Natkunam <Parthipan-Natkunam@users.noreply.github.com>
2016-06-01 22:51:47 +0530

    Debugged version

--- src/RandomPalette.py
+++ src/RandomPalette.py
@@ -5,12 +5,12 @@ class RandomPalette:
         self.RGB =""
         self.C_list = [None]*256
         for C in range(256):
-            C_list[C] = C
+            self.C_list[C] = C
     #Random Pallette Generator
     def randomcolors(self):
-        R = hex(random.choice(C_list))[2:]
-        G = hex(random.choice(C_list))[2:]
-        B = hex(random.choice(C_list))[2:]
+        R = hex(random.choice(self.C_list))[2:]
+        G = hex(random.choice(self.C_list))[2:]
+        B = hex(random.choice(self.C_list))[2:]
         if(len(R)==1):
             R = "0" + R
         if(len(G)==1):
@@ -22,7 +22,7 @@ class RandomPalette:
     
     #Grey Pallette Generator
     def greycolors(self):
-        R = hex(random.choice(C_list))[2:]
+        R = hex(random.choice(self.C_list))[2:]
         if(int(R,16)>1):
             G = hex(int(R,16)-2)[2:]
         else:
